edf7a67d0b554802c8b194b72113134f766d9474
Piotr Rebizant <piotr.rebizant77@gmail.com>
2024-04-16 22:29:18 +0200

    Day2 of Daily Byte

---
+++ Day2.cs
@@ -0,0 +1,13 @@
+﻿string palindrome = Console.ReadLine().Replace(" ", "").ToLower();
+if (palindrome is not null)
+{
+    for (int i = 0; i < palindrome.Length / 2; i++)
+    {
+        if (palindrome[i] != palindrome[palindrome.Length - i - 1])
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+return 0;
